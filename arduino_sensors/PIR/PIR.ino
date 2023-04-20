0795d07512ba85307ace5316748deca06ed7aba2
Jared Ni <milkteadj@gmail.com>
2023-04-20 15:48:49 -0400

    passive infrared sensor working

---
+++ arduino_sensors/PIR/PIR.ino
@@ -0,0 +1,16 @@
+#define SENSOR_PIN 2
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(SENSOR_PIN, INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // if sensorValue == 1 (HIGH) if detect motion
+  int sensorValue = digitalRead(SENSOR_PIN);
+  Serial.println(sensorValue);
+  delay(2000);
+}
