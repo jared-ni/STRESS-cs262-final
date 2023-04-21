3815c334645b26311c7dc8ff22d746a6b6107241
Jared Ni <milkteadj@gmail.com>
2023-04-20 20:10:15 -0400

    they work in the same circuit

---
+++ arduino_sensors/PIR_and_ultrasonic/PIR_and_ultrasonic/PIR_and_ultrasonic.ino
@@ -0,0 +1,51 @@
+/*
+  Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+
+  by Dejan Nedelkovski,
+  www.HowToMechatronics.com
+
+*/
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+
+#define SENSOR_PIN 2
+
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  // PIR
+  pinMode(SENSOR_PIN, INPUT);
+
+  Serial.begin(9600); // Starts the serial communication
+}
+
+
+void loop() {
+   // if sensorValue == 1 (HIGH) if detect motion
+  int sensorValue = digitalRead(SENSOR_PIN);
+  Serial.print("PIR: ");
+  Serial.println(sensorValue);
+
+  
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delay(200);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+}
