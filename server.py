a9c74727dd4ae88a77fee9ecee2878c9b61a00e0
Bryan Han <bryan.han77@gmail.com>
2023-04-22 18:22:24 -0400

    added jess's server stuff

--- server.py
+++ server.py
@@ -2,40 +2,41 @@ import grpc
 from concurrent import futures
 import time
 
-import scheduler_pb2
-import scheduler_pb2_grpc
+import train_pb2
+import train_pb2_grpc
 
-class SchedulerImpl(scheduler_pb2_grpc.SchedulerServicer):
+class Server(train_pb2_grpc.SchedulerServicer):
     def __init__(self):
         self.trains = {}  # Dictionary to store train status
-
+    
     def GetTrainStatus(self, request, context):
         train_id = request.train_id
         if train_id not in self.trains:
             context.abort(grpc.StatusCode.NOT_FOUND, f'Train {train_id} not found')
         
         train_status = self.trains[train_id]
-        return scheduler_pb2.TrainStatusResponse(
-            train_id=train_id,
-            location=train_status.location,
-            speed=train_status.speed,
-            distance_to_stop=train_status.distance_to_stop
-        )
+        return train_status
 
     def UpdateTrainStatus(self, request, context):
         train_id = request.train_id
-        self.trains[train_id] = TrainStatus(
+        self.trains[train_id] = train_pb2.TrainStatusResponse(
+            train_id=train_id,
             location=request.location,
-            speed=request.speed
+            speed=request.speed,
         )
-        # Check for trains too close to each other and send slow down signals
-        # ...
+        return train_pb2.TrainUpdateResponse(success=True)
 
-        return scheduler_pb2.TrainUpdateResponse(success=True)
+    def GetOtherTrainStatus(self, request, context):
+        other_train_id = request.other_train_id
+        if other_train_id not in self.trains:
+            return train_pb2.TrainStatusResponse(train_id=-1)  # Return "not found" status
+
+        train_status = self.trains[other_train_id]
+        return train_status
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    scheduler_pb2_grpc.add_SchedulerServicer_to_server(SchedulerImpl(), server)
+    train_pb2_grpc.add_SchedulerServicer_to_server(Server(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     print('Scheduler API started...')
@@ -46,4 +47,5 @@ def serve():
     except KeyboardInterrupt:
         server.stop(0)
 
-if __name
+if __name__ == '__main__':
+    serve()
