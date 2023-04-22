a97d39358a2ad4fd00418d2a9a755f6e4fc7bfb9
Bryan Han <bryan.han77@gmail.com>
2023-04-22 18:45:31 -0400

    jess server

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ import grpc
 from concurrent import futures
 import time
 
-import train_pb2
-import train_pb2_grpc
+import sys
+sys.path.append("client_sensors")
+import sensor_pb2
+import sensor_pb2_grpc
 
-class Server(train_pb2_grpc.SchedulerServicer):
+class Server(sensor_pb2_grpc.SchedulerServicer):
     def __init__(self):
         self.trains = {}  # Dictionary to store train status
     
@@ -19,24 +21,24 @@ class Server(train_pb2_grpc.SchedulerServicer):
 
     def UpdateTrainStatus(self, request, context):
         train_id = request.train_id
-        self.trains[train_id] = train_pb2.TrainStatusResponse(
+        self.trains[train_id] = sensor_pb2.TrainStatusResponse(
             train_id=train_id,
             location=request.location,
             speed=request.speed,
         )
-        return train_pb2.TrainUpdateResponse(success=True)
+        return sensor_pb2.TrainUpdateResponse(success=True)
 
     def GetOtherTrainStatus(self, request, context):
         other_train_id = request.other_train_id
         if other_train_id not in self.trains:
-            return train_pb2.TrainStatusResponse(train_id=-1)  # Return "not found" status
+            return sensor_pb2.TrainStatusResponse(train_id=-1)  # Return "not found" status
 
         train_status = self.trains[other_train_id]
         return train_status
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    train_pb2_grpc.add_SchedulerServicer_to_server(Server(), server)
+    sensor_pb2_grpc.add_SchedulerServicer_to_server(Server(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     print('Scheduler API started...')
