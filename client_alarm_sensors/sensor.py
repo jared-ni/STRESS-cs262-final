d4db52008daceaa04c0d932e4f6ec5e5de882b43
Bryan Han <bryan.han77@gmail.com>
2023-04-22 00:01:57 -0400

    stuff?

--- client_alarm_sensors/sensor.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-devices = [serial.Serial("/dev/tty.usbmodem101", 9600)]
- 
-while True:
-    for device in devices:
-        line = device.readline().decode('utf-8').rstrip()
-        print(line)
