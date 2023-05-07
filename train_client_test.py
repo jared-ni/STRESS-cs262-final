d4270494ed1cd8bb18c250a173736ccf7d51d939
Bryan Han <bryan.han77@gmail.com>
2023-05-06 22:40:09 -0400

    train tests done

--- train_client_test.py
+++ train_client_test.py
@@ -14,33 +14,33 @@ import threading
 import sys
 
 class TestTrainClient(unittest.TestCase):
-    @classmethod
-    @mock.patch('train_client.input', create=True)
-    def setUpClass(self, mock_input):
-        # server_thread = threading.Thread(target=server.serve, daemon=True)
-        # server_thread.start()
-
-        time.sleep(2)
-        self.train_client_test = train_client.TrainClient()
-        self.train_client_test.train_id = 1
-        
 
     @mock.patch('train_client.input', create=True)
     def test_signup(self, mock_input):       
-        # while self.train_client_test.train_id is None:
+        server_thread = threading.Thread(target=server.serve, daemon=True)
+        server_thread.start()
+        time.sleep(2)
+        self.train_client_test = train_client.TrainClient()
+        self.train_client_test.train_id = 3
+
         reply = self.train_client_test.signup(self.train_client_test.train_id)
         self.train_client_test.thread()
         self.assertEqual(reply.success, True)
-        #     if reply.success:
-        #         print("Valid train ID!")
-        #     else:
-        #         print("{}".format(reply.error))
-        # else:
-        #     print("Invalid train ID. Please enter 1, 2, or 3.")
-        # self.train_client_test.thread()
+
 
     @mock.patch('train_client.input', create=True)
-    def test_update_and_get_status(self, mock_input):       
+    def test_update_and_get_status(self, mock_input):
+
+        server_thread = threading.Thread(target=server.serve, daemon=True)
+        server_thread.start()
+
+        time.sleep(2)
+
+        self.train_client_test = train_client.TrainClient()
+        self.train_client_test.train_id = 1
+
+        reply = self.train_client_test.signup(self.train_client_test.train_id)
+        self.train_client_test.thread()    
         while not self.train_client_test.is_safe_to_instantiate():
             time.sleep(1)
         train_status = self.train_client_test.update_status()
@@ -48,16 +48,23 @@ class TestTrainClient(unittest.TestCase):
         train_status = self.train_client_test.get_status()
         self.assertEqual(train_status.train_id, 1)
 
-
-
-
-
     @mock.patch('train_client.input', create=True)
-    def test_signout(self, mock_input):       
-        # while self.train_client_test.train_id is None:
-        temp = self.train_client_test.signout(self.train_client_test.train_id)
-        self.assertEqual(temp.success, True)
+    def test_get_other_train_status(self, mock_input):
 
+        server_thread = threading.Thread(target=server.serve, daemon=True)
+        server_thread.start()
+
+        time.sleep(2)
+
+        self.train_client_test = train_client.TrainClient()
+        self.train_client_test.train_id = 1
+
+        reply = self.train_client_test.signup(self.train_client_test.train_id)
+        self.train_client_test.thread()    
+
+        # get_other_train_status is called by and is only used for is_safe_to_instantiate
+        status = self.train_client_test.is_safe_to_instantiate()
+        self.assertEqual(status, True)
     
 if __name__ == '__main__':
     unittest.main()
